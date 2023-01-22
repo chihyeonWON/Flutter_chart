c9835a5678b20f73384defab925a8123c4108647
ylose <heungeob1003@gmail.com>
2023-01-22 13:52:07 +0900

    기본 선 그래프 코드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -35,18 +35,24 @@ class MyHomePage extends StatelessWidget {
             width: double.infinity,
             height: 400,
             child:LineChart(
-              LineChartData(borderData: FlBorderData(show: false), lineBarsData: [
-                LineChartBarData(spots: [
-                  const FlSpot(0, 1),
-                  const FlSpot(1, 3),
-                  const FlSpot(2, 10),
-                  const FlSpot(3, 7),
-                  const FlSpot(4, 12),
-                  const FlSpot(5, 13),
-                  const FlSpot(6, 17),
-                  const FlSpot(7, 15),
-                  const FlSpot(8, 20)
-                ])
+              LineChartData(
+                gridData: FlGridData(
+                    show: true,
+                    drawVerticalLine: true,
+                ),
+                  borderData: FlBorderData(show: false),
+                  lineBarsData: [
+                    LineChartBarData(spots: [
+                      const FlSpot(0, 1),
+                      const FlSpot(1, 3),
+                      const FlSpot(2, 10),
+                      const FlSpot(3, 7),
+                      const FlSpot(4, 12),
+                      const FlSpot(5, 13),
+                      const FlSpot(6, 17),
+                      const FlSpot(7, 15),
+                      const FlSpot(8, 20)
+                 ]),
               ]),
             ),
           ),
