788dd755089625f83c554f5f6f0b58e63d9bf71f
ylose <heungeob1003@gmail.com>
2023-01-22 14:21:26 +0900

    두 번째 화면에 랜덤 좌표인 3개의 곡선 그래프를 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // main.dart
 import 'package:flutter/material.dart';
+import 'dart:math';
 import 'package:fl_chart/fl_chart.dart';
 
 void main() {
@@ -73,12 +74,63 @@ class MyHomePage extends StatelessWidget {
 }
 
 class SecondPage extends StatelessWidget {
+  // Generate some dummy data for the cahrt
+  // This will be used to draw the red line
+  final List<FlSpot> dummyData1 = List.generate(8, (index) {
+    return FlSpot(index.toDouble(), index * Random().nextDouble());
+  });
+
+  // This will be used to draw the orange line
+  final List<FlSpot> dummyData2 = List.generate(8, (index) {
+    return FlSpot(index.toDouble(), index * Random().nextDouble());
+  });
+
+  // This will be used to draw the blue line
+  final List<FlSpot> dummyData3 = List.generate(8, (index) {
+    return FlSpot(index.toDouble(), index * Random().nextDouble());
+  });
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title:Text('Multi-Line Chart With Curved Lines'),
+      appBar: AppBar(
+        title: Text('Multi-Line Chart With Curved Lines'),
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          width: double.infinity,
+          child: LineChart(
+            LineChartData(
+              borderData: FlBorderData(show: false),
+              lineBarsData: [
+                // The red line
+                LineChartBarData(
+                  spots: dummyData1,
+                  isCurved: true,
+                  barWidth: 3,
+                  color: Colors.red,
+                ),
+                // The orange line
+                LineChartBarData(
+                  spots: dummyData2,
+                  isCurved: true,
+                  barWidth: 3,
+                  color: Colors.orange,
+
+                ),
+                // The blue line
+                LineChartBarData(
+                  spots: dummyData3,
+                  isCurved: false,
+                  barWidth: 3,
+                  color:Colors.blue,
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
